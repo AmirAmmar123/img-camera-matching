c4befe908ebce1e62f88040e7363b30be0006e17
Amir Ammar <amir.ammar.work@gmail.com>
2024-09-29 01:11:23 +0300

    new CartesianCorrelation class added

--- main.py
+++ main.py
@@ -19,16 +19,12 @@ logging.basicConfig(level=logging.INFO,  # Set level to INFO to capture all INFO
 
 if __name__ == '__main__':
     logging.info('Entering main function...')
-
-   
     matcher = CameraImageMatcher(CameraImageMatcher.parse_arguments())
     logging.info("CameraImageMatcher initialized successfully.")
-
     matcher.run()
-
     logging.info("Loading all gaussian's pairs")
     allgaussianPairs = LoadAllPairsOfGaussian()
-    for x in range(6):
+    for x in range(NUMS_OF_DATAT_SET):
         logging.info("Loading gaussian's pair")
         g = LoadGaussianPair(**allgaussianPairs.get_gaussian(x))
         path = GAUSSIANPLOTSTHEORY + '_'.join(g.axis_1.split('/')[-1:])+'$'+'_'.join(g.axis_2.split('/')[-1:])
